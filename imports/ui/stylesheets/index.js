fef6590c7bea35ee955799c695dbeae0a7130818
danCastro <dcostadcastro@gmail.com>
2017-04-27 17:57:29 -0300

    Home v.1.2

---
+++ imports/ui/stylesheets/index.js
@@ -0,0 +1,13 @@
+import './bootstrap-theme.css';
+
+import './icomoon-social.css';
+import './leaflet.css';
+import './leaflet.ie.css';
+import './lesshat.less';
+
+import './coming-soon-social.css';
+import './general.less';
+import './variables.less';
+import './components.css';
+import './main.less';
+import './main.css';
