e3a7cf8f0b3727fc69716fc3e0d63df8d7ca5493
ChrisCWerner <ccwerner96@gmail.com>
2017-04-28 18:44:58 -0300

    Fixtures pra roles

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,1 +1,32 @@
 // Fill the DB with example data on startup
+
+import { Meteor } from 'meteor/meteor';
+import { Pontos } from '../../api/pontos/pontos.js';
+
+Meteor.startup(() => {
+    if (Meteor.users.find().count() === 0) {
+
+        var admin = {
+            username: "admin",
+            mail: "ad@min.com",
+            password: "gartinhos"
+        };
+        Accounts.createUser(admin);
+        admin = Accounts.findUserByUsername("admin");
+
+        Roles.addUsersToRoles(admin._id, 'admin', Roles.GLOBAL_GROUP);
+
+        var user = {
+            username: "username",
+            mail: "e@mail.com",
+            password: "password"
+        };
+        Accounts.createUser(user);
+        user = Accounts.findUserByUsername("username");
+ 
+        Roles.addUsersToRoles(admin._id, 'um-role-que-nao-existe', Roles.GLOBAL_GROUP);
+
+        console.log(Meteor.users.find().count() + " eh o admin e o user");
+    }
+
+});
