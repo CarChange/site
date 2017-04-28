7fe0434e677a9ac9c256ab18496056daba2dcc03
ChrisCWerner <ccwerner96@gmail.com>
2017-04-28 17:04:53 -0300

    site v0.32 - Essas rotas

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,11 +6,12 @@ import '/imports/ui/layouts/index/index.js';
 //import '/imports/ui/pages/carrinho/carrinho.js';
 import '/imports/ui/pages/home/home.js';
 import '/imports/ui/pages/loading/loading.js';
-//import '/imports/ui/pages/login/login.js';
-//import '/imports/ui/pages/loja/loja.js';
+import '/imports/ui/pages/login/login.js';
+import '/imports/ui/pages/loja/loja.js';
 import '/imports/ui/pages/not-found/not-found.js';
 import '/imports/ui/pages/pontoVirtual/pontoVirtual.js';
 import '/imports/ui/pages/registrar/registrar.js';
+import '/imports/ui/pages/vantagens/vantagens.js';
 
 // Set up fixed Layout
 Router.configure({
@@ -18,17 +19,22 @@ Router.configure({
   notFoundTemplate: 'App_notFound',
   loadingTemplate: 'loading',
 });
+
 // Set up all routes in the app
 Router.route("/", {
   name: "home",
   template:"home",
-
 });
 
-Router.route("/registrar", {
-  name:"registrar",
-  template:"registrar",
-});
+Router.route("/sobre");
+
+Router.route("/registrar");
+
+Router.route("/loja");
+
+Router.route("/login");
+
+Router.route("/vantagens");
 
 Router.route("/pontoVirtual", {
   waitOn:function(){
