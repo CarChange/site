eae0e81a172820606fc39f84ee4956a4f2cb6ec4
ChrisCWerner <ccwerner96@gmail.com>
2017-05-02 16:40:26 -0300

    bora bora

--- imports/ui/components/ponto/pontoAdmin.js
+++ imports/ui/components/ponto/pontoAdmin.js
@@ -1,17 +1,17 @@
 import './pontoAdmin.html';
 import './user.js';
-import {UserData} from '/imports/api/userData/userData.js';
+import { Users } from '/imports/api/users/users.js';
 import { Meteor } from 'meteor/meteor';
 
 
 Template.body.onCreated(function bodyOnCreated() {
   // this.state = new ReactiveDict();
-  Meteor.subscribe("userData");
+  Meteor.subscribe("users");
 });
 
 Template.pontoAdmin.helpers({
   users() {
-      return UserData.find({});
+      return Users.find({});
   },
 
 });
