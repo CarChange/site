8adc145c7dfea0f76e041ef3961392068cec933d
danCastro <dcostadcastro@gmail.com>
2017-05-03 15:57:28 -0300

    Rotas para ADM

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 // Import needed templates
 import '/imports/ui/layouts/index/index.js';
 import '/imports/ui/layouts/index2/index2.js';
+import '/imports/ui/layouts/adm/adm.js';
 //import '/imports/ui/pages/carrinho/carrinho.js';
 import '/imports/ui/pages/home/home.js';
 import '/imports/ui/pages/loading/loading.js';
@@ -15,6 +16,11 @@ import '/imports/ui/pages/registrar/registrar.js';
 import '/imports/ui/pages/sobre/sobre.js';
 import '/imports/ui/pages/vantagens/vantagens.js';
 import '/imports/ui/pages/planos/planos.js';
+import '/imports/ui/pages/admconsass/admconsass.js';
+import '/imports/ui/pages/admhomeadm/admhomeadm.js';
+import '/imports/ui/pages/admhomeass/admhomeass.js';
+import '/imports/ui/pages/admhomeparc/admhomeparc.js';
+
 // Set up fixed Layout
 Router.configure({
   layoutTemplate: 'App_body',
@@ -68,4 +74,28 @@ Router.route("/pontoVirtual", {
   waitOn:function(){
     return Meteor.subscribe('pontos');
   },
+});
+
+Router.route("/admhomeass",
+  function () {
+    this.layout('Adm_body');
+    this.render('admhomeass');
+});
+
+Router.route("/admhomeparc",
+  function () {
+    this.layout('Adm_body');
+    this.render('admhomeparc');
+});
+
+Router.route("/admhomeadm",
+  function () {
+    this.layout('Adm_body');
+    this.render('admhomeadm');
+});
+
+Router.route("/admconsass",
+  function () {
+    this.layout('Adm_body');
+    this.render('admconsass');
 });
