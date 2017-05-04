12b4fd79c0eb05ec70e73c5a876ff4e61ecf08ef
ChrisCWerner <ccwerner96@gmail.com>
2017-05-04 18:34:21 -0300

    cadcarro talvez funcionando

--- imports/ui/components/cadcarro/cadcarro.js
+++ imports/ui/components/cadcarro/cadcarro.js
@@ -1,1 +1,32 @@
 import './cadcarro.html';
+
+Template.cadcarro.events({
+    "submit .form-cadcarro": function(event, template){
+        event.preventDefault();
+
+        var target = event.target;
+
+        var novoCarro = {
+            marca: target.marca.value,
+            modelo: target.modelo.value,
+            categoria: target.categoria.value,
+            valor: target.valor.value,
+            creator: {
+                createdAt: new Date(),
+                adminId: Meteor.userId(),
+                //TODO FAZER CREATOR NO SERVIDOR
+            }
+        }
+
+        Meteor.call("carros.insert", novoCarro, (error, result) => {
+            if(error) {
+                // swal(error.reason, error.details, 'error');
+            }
+            if (result) {
+                swal("Foi!", "Carro cadastrado com sucesso.");
+            }
+        });
+
+        template.find(".form-cadcarro").reset();
+    }
+});
