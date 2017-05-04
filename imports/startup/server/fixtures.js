e1f8a164930b6956fe88b0debe2aac6e1bfdc1a8
ChrisCWerner <ccwerner96@gmail.com>
2017-05-04 15:36:37 -0300

    fix em fixtures x

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -4,29 +4,16 @@ import { Meteor } from 'meteor/meteor';
 import { Pontos } from '../../api/pontos/pontos.js';
 
 Meteor.startup(() => {
-    if (Meteor.users.find().count() === 0) {
+    if (Accounts.findUserByEmail("ad@min.com")) {
 
         var admin = {
-            username: "admin",
             mail: "ad@min.com",
             password: "gartinhos"
         };
         Accounts.createUser(admin);
-        admin = Accounts.findUserByUsername("admin");
+        admin = Accounts.findUserByEmail("ad@min.com");
 
         Roles.addUsersToRoles(admin._id, 'admin', Roles.GLOBAL_GROUP);
-
-        var user = {
-            username: "username",
-            mail: "e@mail.com",
-            password: "password"
-        };
-        Accounts.createUser(user);
-        user = Accounts.findUserByUsername("username");
-
-        Roles.addUsersToRoles(user._id, ['um-role-que-nao-existe', 'user'], Roles.GLOBAL_GROUP);
-
-        console.log(Meteor.users.find().count() + " eh o admin e o user");
     }
 
 });
