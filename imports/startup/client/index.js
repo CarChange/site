cd485dca76ab370990b1cd95e0514b2008529228
Lukas <kukafernando@hotmail.com>
2017-05-04 15:36:23 -0300

    Configuração de Captcha

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,11 +1,11 @@
 // Import client startup through a single index entry point
-
 import './routes.js';
 import './accounts-config.js';
+
 if(Meteor.isClient){
   Meteor.startup(function() {
       reCAPTCHA.config({
-          publickey: '6Lexwh8UAAAAAKej41dKKbr2hBWBiTFuaOb60AnN',
+          publickey: '6Lf6-R8UAAAAAH7XBcGz2ZtAqts2_555PGo53z6B',
           hl: 'pt-BR' // optional display language
       });
   });
