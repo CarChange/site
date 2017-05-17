8f0271aac6c33d8cb377047b000bfde24d118211
danCastro <dcostadcastro@gmail.com>
2017-05-17 18:27:48 -0300

    navbar pronta :)

--- imports/ui/stylesheets/index.js
+++ imports/ui/stylesheets/index.js
@@ -1,11 +1,6 @@
-import './bootstrap-theme.css';
-
-import './icomoon-social.css';
 import './leaflet.css';
 import './leaflet.ie.css';
 
-
-import './coming-soon-social.css';
 import './variables.less';
 import './general.less';
 import './components.less';
