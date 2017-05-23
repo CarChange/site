a42ec1d13e0624381f220bcca9f34069104fb799
ChrisCWerner <ccwerner96@gmail.com>
2017-05-23 14:22:23 -0300

    mail_url redefinido

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -8,14 +8,4 @@ if(Meteor.isServer){
       reCAPTCHA.config({
           privatekey: '6Lf6-R8UAAAAAFWJpW4OOPMLz3qCujboWIzHSIc5'
       });
-
-      //Setando o MAIL_URL TODO Configurar como environment variable
-      smtp = {
-          username: 'postmaster@mg.carchange.com.br',
-          password: 'd5f28ee257fa193da9da6bed9bfeb181',
-          server: 'smtp.mailgun.org',
-          port: 465 //alternar entre 465 (SSL) e 587 (TLS)
-      }
-      process.env.MAIL_URL = "smtps://" + encodeURIComponent(smtp.username) + ":" + encodeURIComponent(smtp.password) + "@" + encodeURIComponent(smtp.server) + ":" + smtp.port;
-  });
 }
