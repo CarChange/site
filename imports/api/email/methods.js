ffa81e9c6a871ef67f6f02b2be40ab2ba69d6ef8
ChrisCWerner <ccwerner96@gmail.com>
2017-05-24 16:08:43 -0300

    edits em verificação de email - testar em deploy

--- imports/api/email/methods.js
+++ imports/api/email/methods.js
@@ -1,6 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import { Email } from 'meteor/email';
 
+Accounts.emailTemplates.siteName = 'CarChange';
+Accounts.emailTemplates.from = 'CarChange <naoresponda@carchange.com.br>';
+
+Accounts.emailTemplates.enrollAccount.subject = (user) => {
+  return `Bem vindo à CarChange, ${user.profile.nome.primeiro}`;
+};
+
+Accounts.emailTemplates.enrollAccount.text = (user, url) => {
+  return 'Cadastro realizado com sucesso! Para ativar sua conta, clique no link abaixo: \n\n'
+    + url;
+};
+
+Accounts.emailTemplates.resetPassword.from = () => {
+  // Overrides the value set in `Accounts.emailTemplates.from` when resetting
+  // passwords.
+  return 'CarChange - Resetar Senha <naoresponda@carchange.com.br>';
+};
+
+Accounts.emailTemplates.verifyEmail = {
+   subject() {
+      return "[CarChange] Ative sua conta agora!";
+   },
+   text(user, url) {
+      return `Olá, ${user.profile.nome.primeiro}! Verifique seu email ao clicar neste link:\n\n ${url}`;
+   }
+};
+
 Meteor.methods({
     sendEmail: function(userEmail){
         Email.send({
@@ -10,5 +37,15 @@ Meteor.methods({
             text: "Bem vindo :)",
         });
         //Invocar funcao callback com error, result ao chamar sendEmail.
+    },
+    'sendVerificationLink'() {
+        let userId = Meteor.userId();
+        if ( userId ){
+            console.log("enviando");
+            return Accounts.sendVerificationEmail( userId );
+        }
+        else
+          throw new Meteor.Error(25, 'Não foi possível enviar o email de verificação.', "Verifique se o usuário está logado.");
+
     }
 });
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -8,7 +8,23 @@ if(Meteor.isServer){
       reCAPTCHA.config({
           privatekey: '6Lf6-R8UAAAAAFWJpW4OOPMLz3qCujboWIzHSIc5'
       });
-
-      process.env.MAIL_URL = "smtps://naoresponda%40carchange.com.br:57LciaThWWgp@smtp.zoho.com:465";
   });
+
+    //Teste para tentar fazer a verificação de email:
+    (function () {
+        "use strict";
+
+        Accounts.urls.resetPassword = function (token) {
+            return Meteor.absoluteUrl('reset-password/' + token);
+        };
+
+        Accounts.urls.verifyEmail = function (token) {
+            return Meteor.absoluteUrl('verify-email/' + token);
+        };
+
+        Accounts.urls.enrollAccount = function (token) {
+            return Meteor.absoluteUrl('enroll-account/' + token);
+        };
+
+    })();
 }
